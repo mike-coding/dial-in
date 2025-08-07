e3bdeddcbd0b41003b4ea680a7838d2be9470b7a
mike-coding <mikebeast27@gmail.com>
2025-08-06 23:13:30 -0500

    drawer fix

--- front-end/src/components/Drawer.tsx
+++ front-end/src/components/Drawer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Page, useNavigationContext } from "../hooks/AppContext";
 import { getVersionString } from '../utils/version';
 
